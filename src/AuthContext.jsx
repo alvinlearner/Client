b703a8ee1c84ae80111e0a6c55c7be1c5bec7c4e
alvinlearner <alvin47k@outlook.com>
2024-01-30 14:51:16 +0300

    Logout change to navigate instead of window.location

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,6 +1,7 @@
 // authContext.js
 import { createContext, useContext, useState, useEffect } from 'react';
 import swal from 'sweetalert';
+import { Navigate } from 'react-router-dom';
 
 const AuthContext = createContext();
 
@@ -65,7 +66,7 @@ export const AuthProvider = ({ children }) => {
         sessionStorage.removeItem('admin');
 
         // Navigate to "/"
-        window.location.href = "/";
+        return <Navigate to="/" />;
       }
     } catch (error) {
       throw new Error('An error occurred during logout');
@@ -88,124 +89,3 @@ export const useAuth = () => {
   return useContext(AuthContext);
 };
 
-
-
-// // authContext.js
-// import { createContext, useContext, useState, useEffect } from 'react';
-// import { Navigate } from 'react-router-dom';
-
-// import swal from 'sweetalert';
-
-// const AuthContext = createContext();
-
-// export const AuthProvider = ({ children }) => {
-//   const [admin, setAdmin] = useState(() => {
-//     // Initialize admin state from localStorage, if available
-//     const storedToken = localStorage.getItem('jwtToken');
-//     return storedToken ? decodeToken(storedToken) : null;
-//   });
-
-//   const login = async ({ username, password }) => {
-//     try {
-
-//       const response = await fetch('https://insurance-u3z5.onrender.com/login', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//           Accept:"application/json",
-//           Authorization: localStorage.token
-//         },
-//         body: JSON.stringify({ username, password }),
-//       });
-      
-
-//       if (!response.ok) {
-//         throw new Error('Invalid login credentials');
-//       }
-
-//       const { admin, jwt } = await response.json();
-//       setAdmin(admin);
-
-//       // Save JWT token to localStorage
-//       localStorage.setItem('jwtToken', jwt);
-
-//       return admin;
-//     } catch (error) {
-//       throw new Error('An error occurred during login');
-//     }
-//   };
-
-
-//   const logout = async () => {
-//         try {
-//           const result = await swal({
-//             title: "Are you sure?",
-//             text: "You will be logged out!",
-//             icon: "warning",
-//             buttons: true,
-//             dangerMode: true,
-//           });
-    
-//           if (result) {
-//             const response = await fetch('https://insurance-u3z5.onrender.com/logout', {
-//               method: 'DELETE',
-//               headers: {
-//                 'Content-Type': 'application/json',
-//               },
-//             });
-    
-//             if (!response.ok) {
-//               throw new Error('Logout failed');
-//             }
-    
-//             setAdmin(null);
-    
-//             // Remove admin data from sessionStorage
-//             localStorage.removeItem('jwtToken');  // Remove admin data from localStorage
-//             // Navigate to "/"
-//             return <Navigate to="/" />;
-//           }
-//         } catch (error) {
-//           throw new Error('An error occurred during logout');
-//         }
-//       };
-
-
-
-
-
-
-
-//   // Rehydrate admin state on component mount
-//   useEffect(() => {
-//     const storedToken = localStorage.getItem('jwtToken');
-//     if (storedToken) {
-//       setAdmin(decodeToken(storedToken));
-//     }
-//   }, []);
-
-//   // Cleanup localStorage on component unmount
-//   useEffect(() => {
-//     return () => localStorage.removeItem('jwtToken');
-//   }, []);
-
-//   return (
-//     <AuthContext.Provider value={{ admin, login, logout }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export const useAuth = () => {
-//   return useContext(AuthContext);
-// };
-
-// // Helper function to decode the JWT token
-// const decodeToken = (token) => {
-//   try {
-//     const decoded = JSON.parse(atob(token.split('.')[1]));
-//     return decoded;
-//   } catch (error) {
-//     return null;
-//   }
-// };
